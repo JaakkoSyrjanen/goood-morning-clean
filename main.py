ab608e11741721721f9a5c8a15642323fe775d91
JaakkoSyrjanen <jaakko.syrjanen70@gmail.com>
2025-05-03 22:34:38 +0300

    Add /auth/ping route

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from routes import auth  # <-- Import our new router
 
 app = FastAPI()
+
+app.include_router(auth.router)  # <-- Register it
 
 @app.get("/")
 def root():
